769a52ba1d4e992611d57fd9a32842381a5f8cf0
homedesktop@2DGP <gunageroo@gmail.com>
2024-11-12 21:56:12 +0900

    Drill11 제출(2023184004 권현아)

--- game_over.py
+++ game_over.py
@@ -12,11 +12,11 @@ def finish():
     global image
     del image
 
-def handle_events():
+def handle_events():  
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            game_framework.quit()  
+            game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:

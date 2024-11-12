cfda5dc1f886c71b529bb9a4df6bb88a2fb94d8e
homedesktop@2DGP <gunageroo@gmail.com>
2024-11-12 21:54:20 +0900

    Drill11 제출(2023184004 권현아)

--- game_over.py
+++ game_over.py
@@ -16,7 +16,7 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            game_framework.quit()
+            game_framework.quit()  
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
